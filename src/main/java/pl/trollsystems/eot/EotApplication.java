15888aa2f8d61ac0c6edc717cc5432fa2e48e2c4
Marek <macki@int.pl>
2021-07-23 23:35:25 +0200

    init repo

---
+++ src/main/java/pl/trollsystems/eot/EotApplication.java
@@ -0,0 +1,13 @@
+package pl.trollsystems.eot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/trollsystems/eot/EotApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.trollsystems.eot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
